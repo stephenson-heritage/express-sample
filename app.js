8aef30a09636ff26d1886372a0e5e257b38dd9aa
stephenson-heritage <lstephenson@cegep-heritage.qc.ca>
2019-04-08 10:04:17 -0400

    class ex

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ const app = express();
 const port = 9000;
 
 app.get("/", (req, res) => res.send("hello!"));
-app.get("/user/:username", (req, res) =>
-	res.send("Welcome : " + req.params.username)
-);
 
 app.listen(port, function() {
 	return console.log("Sample App " + port + "!");
