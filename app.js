4d4ed21b49a5a238a2a4b91db7121e242000fcea
stephenson-heritage <lstephenson@cegep-heritage.qc.ca>
2019-04-08 11:54:00 -0400

    for straggler

--- app.js
+++ app.js
@@ -2,7 +2,37 @@ const express = require("express");
 const app = express();
 const port = 9000;
 
-app.get("/", (req, res) => res.send("hello!"));
+app.get("/bye", (req, res) => {
+	return res.send("bubye!");
+});
+
+app.get("/hello", (req, res) => res.send("hey there"));
+
+app.get("/users/:userId/:email", (req, res) => {
+	let uid = parseInt(req.params.userId);
+	let email = req.params.email;
+
+	res.send(`user ${uid}'s email set to ${email}`);
+});
+
+app.get("/numbers/:from-:to", (req, res) => {
+	let a = parseInt(req.params.from);
+	let b = parseInt(req.params.to);
+
+	let html = "";
+
+	if (a > b) {
+		let t = a;
+		a = b;
+		b = t;
+	}
+
+	for (let x = a; x <= b; x++) {
+		html += `<div>${x}</div>`;
+	}
+
+	res.send(html);
+});
 
 app.listen(port, function() {
 	return console.log("Sample App " + port + "!");
